3aa21f86210b7606775e5641e89153e35db7cac1
slnna <74587060+aka-dag@users.noreply.github.com>
2026-05-10 19:56:24 +0800

    Refactor buildPropertyValue and add select support
    Refactor buildPropertyValue function to simplify object creation and add support for NotionSelectPropType.

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,7 @@ import {
   type NotionRichTextPropType,
   type NotionTitlePropType,
   type NotionFilesPropType,
+  type NotionSelectPropType,
   type NotionDatePropType,
   type NotionMultiSelectPropType,
   type NotionNumberPropType,
@@ -15,12 +16,6 @@ import {
 
 dotenv.config();
 
-/**
- * Retrieves the data source ID for the given category.
- *
- * @param {ItemCategory} category - The category of the item.
- * @returns {string} The corresponding database ID.
- */
 export function getDataSourceId(category: ItemCategory): string {
   const databasesMap = {
     [ItemCategory.Movie]: process.env.NOTION_MOVIE_DATABASE_ID,
@@ -32,81 +27,41 @@ export function getDataSourceId(category: ItemCategory): string {
   return databasesMap[category] as string;
 }
 
-/**
- * Delays the execution of a function by a specified number of milliseconds.
- *
- * @param {number} ms - The number of milliseconds to sleep
- * @return {Promise<void>} A Promise that resolves after the specified number of milliseconds
- */
 export function sleep(ms: number): Promise<void> {
   return new Promise((r) => setTimeout(r, ms));
 }
 
-/**
- * Generates the value for a property based on the given parameters, ready to insert
- * into notion database.
- *
- * @param {any} value - the value to be processed
- * @param {NotionPropTypesEnum} type - the type of the Notion database property
- * @param {string} key - the key associated with the property
- * @return {any} the generated value for the property later will be sent to notion to create an item
- */
 export function buildPropertyValue(value: any, type: NotionPropTypesEnum, key: string): NotionColPropTypes | undefined {
   switch (type) {
     case NotionPropTypesEnum.TITLE:
       return {
         type: NotionPropTypesEnum.TITLE,
-        title: [
-          {
-            text: {
-              content: value,
-            },
-          },
-        ],
+        title: [{ text: { content: value } }],
       } as NotionTitlePropType;
     case NotionPropTypesEnum.FILES:
       return {
         type: NotionPropTypesEnum.FILES,
-        files: [
-          {
-            name: value,
-            type: 'external',
-            external: {
-              url: value
-            },
-          },
-        ],
+        files: [{ name: value, type: 'external', external: { url: value } }],
       } as NotionFilesPropType;
+    case NotionPropTypesEnum.SELECT:
+      return {
+        type: NotionPropTypesEnum.SELECT,
+        select: value ? { name: value.toString() } : null,
+      } as NotionSelectPropType;
     case NotionPropTypesEnum.DATE:
       return {
         type: NotionPropTypesEnum.DATE,
-        date: {
-          start: value,
-        },
+        date: { start: value },
       } as NotionDatePropType;
     case NotionPropTypesEnum.MULTI_SELECT:
-      return key === DB_PROPERTIES.RATING
-        ? {
-          type: NotionPropTypesEnum.MULTI_SELECT,
-          multi_select: value
-            ? [{ name: value.toString() }]
-            : [],
-        } as NotionMultiSelectPropType
-        : {
-          type: NotionPropTypesEnum.MULTI_SELECT,
-          multi_select: (value || []).map((g: string) => ({ name: g })),
-        } as NotionMultiSelectPropType;
+      return {
+        type: NotionPropTypesEnum.MULTI_SELECT,
+        multi_select: (value || []).map((g: string) => ({ name: g })),
+      } as NotionMultiSelectPropType;
     case NotionPropTypesEnum.RICH_TEXT:
       return {
         type: NotionPropTypesEnum.RICH_TEXT,
-        rich_text: [
-          {
-            type: 'text',
-            text: {
-              content: value || '',
-            },
-          },
-        ],
+        rich_text: [{ type: 'text', text: { content: value || '' } }],
       } as NotionRichTextPropType;
     case NotionPropTypesEnum.NUMBER:
       return {
